64fc1cf32401d2631d4e46e1098028799e24502f
kammy <kamilromasz@hotmail.com>
2023-03-04 14:09:21 +0100

    basic player animation

--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
 from pygame.math import Vector2 as vector
+from os import walk
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, path, collision_sprites):
         super().__init__(groups)
-        self.image = pygame.Surface((100, 100))
-        self.image.fill('red')
+        self.import_assets(path)
+        self.frame_index = 0
+        self.status = 'down_idle'
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
         # movement
@@ -17,21 +20,47 @@ class Player(pygame.sprite.Sprite):
         self.hitbox = self.rect.inflate(0, - self.rect.height/2)
         self.collision_sprites = collision_sprites
 
+    def import_assets(self, path):
+        self.animations = {}
+
+        for index, folder in enumerate(walk(path)):
+            if index == 0:
+                for name in folder[1]:
+                    self.animations[name] = []
+            else:
+                for file_name in sorted(folder[2], key=lambda string: int(string.split(".")[0])):
+                    path = folder[0].replace('\\', '/') + '/' + file_name
+                    surf = pygame.image.load(path).convert_alpha()
+                    key = folder[0].split("\\")[1]
+                    self.animations[key].append(surf)
+
     def input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
+
+    def animate(self, dt):
+        current_animation = self.animations[self.status]
+
+        self.frame_index += 7 * dt
+        if self.frame_index >= len(current_animation):
+            self.frame_index = 0
+        self.image = current_animation[int(self.frame_index)]
 
     def move(self, dt):
         # normalize vector
@@ -51,3 +80,4 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
